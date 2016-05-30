540d1d27fe380bb8e543ee21b35128fdb9d893c9
Sheikh Ashiqul Islam Pollob <pollob60@gmail.com>
2016-05-31 00:20:43 +0600

    C# Code wars Solutions

---
+++ Codewars/Return the Missing Element/Return the Missing Element/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_the_Missing_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] superImportantArray = new int[] { 0, 5, 1, 3, 2, 9, 7, 6, 4 };
+            //int missingVal = GetMissingElement(superImportantArray);
+            int missingVal = GetMissingElementSort(superImportantArray);
+            Console.WriteLine(missingVal);
+        }
+
+        private static int GetMissingElementSort(int[] superImportantArray)
+        {
+            Array.Sort(superImportantArray);
+
+            int index = 0;
+            while (superImportantArray[index] == index) {
+                index++;
+            }
+
+            return index;
+        }
+
+        static int GetMissingElement(int[] superImportantArray)
+        {
+
+            bool[] missingValLog = new bool[] { true, true, true, true, true, true, true, true, true, true};
+            for (int i = 0; i < superImportantArray.Length; i++ )
+            {
+                missingValLog[superImportantArray[i]] = false;
+            }
+
+            for (int i = 0; i < missingValLog.Length; i++ )
+            {
+                if(missingValLog[i]){
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Codewars/Sum Strings as Numbers/UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sum_Strings_as_Numbers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("-1", Program.ExcludeLeadingZeros("-001"));
+        }
+    }
+}
