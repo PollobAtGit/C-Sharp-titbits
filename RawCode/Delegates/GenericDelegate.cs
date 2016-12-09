b137cf5585694e58bc21d8afe29d0a903435c47a
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2016-12-09 12:48:19 +0600

    Generic delegate
    - Map strongely typed method to generic delegate
    - Primitive type array can't be converted to reference array

--- RawCode/Delegates/GenericDelegate.cs
+++ RawCode/Delegates/GenericDelegate.cs
@@ -9,13 +9,17 @@ public class Program
 
     public static void Main()
     {
-        //Note the usage. Delegate is a .NET type. This is usage is similar to generic class declaration
+        //POI: Note the usage. Delegate is a .NET type. This is usage is similar to generic class declaration
+        //POI: Max/Min are generic methods themselves
+        //POI: Because of this declaration Max & Min's type parameter is IComparable
         ArithmeticOperation<IComparable> op = Max;
         op += Min;
         
         IterateOverInvocationList(op);
     }
     
+    //POI: Similar to delegate instance declaration, parameter must have a type defined
+    //This has to be resolved at compile time
     private static void IterateOverInvocationList(ArithmeticOperation<IComparable> op)
     {
         foreach (var method in op.GetInvocationList())
---
+++ RawCode/Delegates/GenericTransform.cs
@@ -0,0 +1,4 @@
+    public delegate T Transformer<T>(T a);
+
+    {
+        var values = new int[] { 0,1, 2, 3 };
