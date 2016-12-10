c31dc9868bf0132ab86a508a9bd1674fb266fabb
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2016-12-10 20:36:40 +0600

    .NET System.Collections overview
    - Iterate by IEnumerable<T>.GetEnumerator()
    - Work on List<T>, Dictionary<T>, Stack<T>, Queue<T>

---
+++ RawCode/Collection/ReadMe.txt
@@ -0,0 +1,49 @@
+
+# .NET collections can be categorized into three types
+    ## Standard protocols/interfaces
+    ## Ready-to use collection class
+    ## Base class to write application specific collections
+
+# .NET collection namespaces:
+    ## System.Collections
+        ### ...
+        ### Generic
+        ### Specialized
+        ### ObjectModel
+        ### Concurrent
+
+# Implementation hierarchy (not exact; in terms of granularity)
+    ## IEnumerator
+    ## IEnumerator<T>   => Also implements IDisposable
+        ### IEnumerable
+        ### IEnumerable<T>
+            #### ICollection
+            #### ICollection<T>
+                ##### IList
+                ##### IList<T>
+
+                ##### IDictionary
+                ##### IDictionary<T>
+
+# IEnumerator interface
+
+{code:C#}
+    public interface IEnumerator
+    {
+        bool MoveNext();
+        void Reset();
+
+        //Only getter is needed because the consumer of the IEnumerator class will access
+        //only the value it doesn't need to SET it
+        object Current { get; }
+    }
+{code}
+
+# IEnumerable interface
+
+{code:C#}
+    public interface IEnumerable
+    {
+        IEnumerator GetEnumerator();
+    }
+{code}
---
+++ RawCode/Collection/UseLowLevelGetEnumerator.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        /********** ITERATING OVER CHAR SEQUENCE *******************/
+
+        string str = "Greeting!";
+
+        IEnumerator<char> enumerator = str.GetEnumerator();
+
+        while(enumerator.MoveNext())
+        {
+            Print<char>(enumerator.Current);
+        }
+
+
+        /********** ITERATING OVER A LINKED LIST OF X *******************/
+
+        LinkedList<X> xLinkedList = new LinkedList<X>();
+        xLinkedList.AddFirst(new X("Hello!"));
+        xLinkedList.AddFirst(new X("Olo!"));
+        xLinkedList.AddFirst(new X("Melo!"));
+        xLinkedList.AddFirst(new X("Hi!"));
+
+        //LinkedList<T> implements IEnumerable<T>
+        IEnumerator<X> xEnumerator = xLinkedList.GetEnumerator();
+
+        //POI: Type cannot be inferred from statement 'Print(null)' because type in this case can be
+        // any reference type of nullable value type
+        Print<string>(null);
+
+        while(xEnumerator.MoveNext())
+        {
+            Print<string>(xEnumerator.Current.Greeting);
+        }
+
+        /********** ITERATING OVER A DICTIONARY OF X *******************/
+
+        Dictionary<string, X> xDic = new Dictionary<string, X>
+        {
+            { "Traditional Greeting", new X("Hello!") },
+            { "Funky Greeting", new X("Olo!") },
+            { "Meaningless Greeting", new X("Melo!") },
+            { "Modern Greeting", new X("Hi!") }
+        };
+
+        IEnumerator<KeyValuePair<string, X>> xDicEnumerator = xDic.GetEnumerator();
+
+        Print<string>(null);
+        while(xDicEnumerator.MoveNext())
+        {
+            KeyValuePair<string, X> xPair = xDicEnumerator.Current;
+            Print<string>("KEY: " + xPair.Key + " || VALUE: " + xPair.Value.Greeting);
+        }
+
+        /********** ITERATING OVER A STACK OF X *******************/
+
+        Stack<X> xStack = new Stack<X>();
+
+        xStack.Push(new X("Hello!"));
+        xStack.Push(new X("Olo!"));
+        xStack.Push(new X("Hi!"));
+
+        IEnumerator<X> xStackEnumerator = xStack.GetEnumerator();
+
+        Print<string>(null);
+        while(xStackEnumerator.MoveNext())
+        {
+            Print<string>(xStackEnumerator.Current.Greeting);
+        }
+
+        /********** ITERATING OVER A QUEUE OF X *******************/
+
+        Queue<X> xQueue = new Queue<X>();
+
+        xQueue.Enqueue(new X("Hello!"));
+        xQueue.Enqueue(new X("Olo!"));
+        xQueue.Enqueue(new X("Hi!"));
+
+        IEnumerator<X> xQueueEnumerator = xQueue.GetEnumerator();
+
+        Print<string>(null);
+        while(xQueueEnumerator.MoveNext())
+        {
+            Print<string>(xQueueEnumerator.Current.Greeting);
+        }
+
+    }
+
+    private class X
+    {
+        public string Greeting { get; private set; }
+
+        //What doesn it mean to have a public member in a private nested class?
+        public X(string greeting)
+        {
+            Greeting = greeting;
+        }
+    }
+
+    public static void Print<T>(T a) => Console.WriteLine(a);
+}
