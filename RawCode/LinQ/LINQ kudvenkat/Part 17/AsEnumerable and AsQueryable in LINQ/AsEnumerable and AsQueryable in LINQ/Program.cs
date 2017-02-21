34bb6bc1086c6a04a29c27bfea858e055bf391b9
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-02-21 22:09:00 +0600

    Linq to SQL vs Linq to Object

---
+++ RawCode/LinQ/LINQ kudvenkat/Part 17/AsEnumerable and AsQueryable in LINQ/AsEnumerable and AsQueryable in LINQ/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AsEnumerable_and_AsQueryable_in_LINQ.Data;
+
+namespace AsEnumerable_and_AsQueryable_in_LINQ
+{
+    internal static class Program
+    {
+        private static readonly EmployeeDbDataContext Ctx;
+
+        static Program()
+        {
+            Ctx = new EmployeeDbDataContext();
+        }
+
+        static void Main(string[] args)
+        {
+            //Poi: This operates as LINQ to SQL
+            IQueryable<Employee> filteredEmployees = Ctx.Employees.Where<Employee>(employee => employee.Salary > 200);
+            filteredEmployees.IterateOverSequence();
+
+            //Poi: This operates as LINQ to objects
+            IEnumerable<Employee> filteredEmployeesEnum = Ctx.Employees
+                .AsEnumerable()
+                .Where<Employee>(employee => employee.Salary > 200);
+
+            filteredEmployeesEnum.IterateOverSequence();
+        }
+    }
+
+    //Poi: Extension method must be defined in a TOP LEVEL class
+    //Poi: Extension method must be declared in a NON-GENERIC static class
+    internal static class QueryableExtension
+    {
+        public static void IterateOverSequence<TModel>(this IEnumerable<TModel> source)
+        {
+            Console.WriteLine();
+            foreach (TModel model in source)
+            {
+                Console.WriteLine(model);
+            }
+        }
+    }
+}
