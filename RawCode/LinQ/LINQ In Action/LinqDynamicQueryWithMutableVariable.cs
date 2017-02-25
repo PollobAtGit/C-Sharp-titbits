84d0a204870e54b8a5a3d65d87c7cb561e6b9797
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-02-25 12:30:53 +0600

    Dynamic Query By LINQ 101

---
+++ RawCode/LinQ/LINQ In Action/LinqDynamicQueryWithMutableVariable.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinqDynamicQueryWithMutableVariable
+{
+    internal static class Client
+    {
+        private static ArrayList Publishers;
+
+        static Client()
+        {
+            Publishers = new ArrayList
+            {
+                new Publisher { Name="Fun Books Publisher", WebSite = "https://www.funbooks.com" },
+                new Publisher { Name="Joe Publishing", WebSite = "https://www.joe-publishing.org" },
+                new Publisher { Name="I Publisher", WebSite = "http://i.org/me" },
+            };
+        }
+
+        public static void Main()
+        {
+            SearchPublisherByDomainName(".org").IterateOverSequence<Publisher>();
+            SearchPublisherByDomainName(".com").IterateOverSequence<Publisher>();
+            SearchPublisherByDomainName(".me").IterateOverSequence<Publisher>();
+
+            //Poi: Consider the above examples here too. In this case it's evident that the local variable value isn't har codedly bound to the
+            //query
+
+            string domainToSearchFor = ".org";
+            IEnumerable<Publisher> dotPublishers = Publishers
+                                                    .OfType<Publisher>()
+                                                    .Where<Publisher>(publisher => publisher.WebSite.Contains(domainToSearchFor));
+
+            dotPublishers.IterateOverSequence<Publisher>();
+
+            domainToSearchFor = ".com";
+            dotPublishers.IterateOverSequence<Publisher>();
+
+            domainToSearchFor = ".me";
+            dotPublishers.IterateOverSequence<Publisher>();
+        }
+
+        //Poi: Filtering is dependent on a local variable which is passed via argument
+        private static IEnumerable<Publisher> SearchPublisherByDomainName(string domainToSearchFor)
+        {
+            return Publishers
+                .OfType<Publisher>()
+                .Where<Publisher>(publisher => publisher.WebSite.Contains(domainToSearchFor));
+        }
+
+        private static void IterateOverSequence<TModel>(this IEnumerable<TModel> source)
+        {
+            if(source == null || !source.Any()) return;
+
+            try
+            {
+                Console.WriteLine();
+                foreach(TModel model in source)
+                {
+                    Console.WriteLine(model);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private class Author
+        {
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public String WebSite { get; set; }
+
+            public override string ToString()
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        private class Publisher
+        {
+            public String Name {get; set;}
+            public String WebSite {get; set;}
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        private interface IBook
+        {
+            string Title { get; set; }
+        }
+
+        private class Fiction : IBook
+        {
+            public string Title { get; set; }
+
+            public override string ToString() => Title;
+        }
+
+        private class NonFiction : IBook
+        {
+            public string Title { get; set; }
+
+            public override string ToString() => Title;
+        }
+    }
+}
