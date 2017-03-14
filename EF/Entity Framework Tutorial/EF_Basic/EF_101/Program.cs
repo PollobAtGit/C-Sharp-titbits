1a11d313951234d86d0e333ce7811bcdfd0ce1a1
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-03-14 23:59:18 +0600

    Get ObjectContext From DbContext

--- EF/Entity Framework Tutorial/EF_Basic/EF_101/Program.cs
+++ EF/Entity Framework Tutorial/EF_Basic/EF_101/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace EF_101
+//Poi: Infrastructure is required when IObjectContextAdapter needs to be accessed to get ObjectContext
+using System.Data.Entity.Infrastructure;
+
+//Poi: ObjectContext resides in this namespace (System.Data.Entity.Core.Objects)
+using System.Data.Entity.Core.Objects;
+
+using EF_101.EDM;
+
+    namespace EF_101
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (SchoolDBEntities ctx = new SchoolDBEntities())
+            {
+                ObjectContext objCtx = (ctx as IObjectContextAdapter).ObjectContext
+            }
         }
     }
 }
