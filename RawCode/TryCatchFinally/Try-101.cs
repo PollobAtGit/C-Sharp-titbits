732b206a406f04db035688d7226826721c9873f3
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-03-21 23:48:12 +0600

    try-catch block creates two entry points for the code block / method - If try block contains a return statement & there is a finally block then contents of finally will be executed first than the return statement will be executed

--- RawCode/TryCatchFinally/Try-101.cs
+++ RawCode/TryCatchFinally/Try-101.cs
@@ -12,11 +12,18 @@ namespace Try_101
             Console.WriteLine(Do(null));
         }
 
+        //Poi: In a method where there is try-block statement, there method leaving point is multiple,
+        //Once via, 'try' & the other is via 'catch'. In both cases, if there is any finally block than
+        //that will be invoked first then the return statement, which makes more sense because in that case
+        //the method will get chance to release valuable resource, if it's holding any
         private static bool Do(object obj)
         {
             try
             {
                 if(obj == null) throw new NotImplementedException();
+
+                //Poi: Before this statement is executed, 'finally' will be invoked
+                return true;
             }
             catch(Exception exp)
             {
@@ -38,9 +45,8 @@ namespace Try_101
                 // return true;
             }
 
-            Console.WriteLine("Released Shared Resources");
-
-            return true;
+            //Poi: This code block is unreachable because 'try' & 'catch' both has related return statement
+            Console.WriteLine("This code is unreachable");
         }
     }
 }
