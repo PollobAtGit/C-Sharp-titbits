0c8b9e795b903771b0cd4d8aed23b8d65243ff96
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-03-23 09:41:54 +0600

    Explore SqlCommand.ExecuteScalar() - SEPERATE connection needs to be opened & closed for SEPERATE SQL operations

---
+++ ADO.NET/MicrosoftActiveXDataObject/ADO_101/ASPXs/Products.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="ADO_101.ASPXs.Products" %>
+
+<asp:Content ID="ProductContent" ContentPlaceHolderID="MainContent" runat="server">
+    <label>Total products count</label>
+    <asp:Label ID="totalProductCount" runat="server"></asp:Label>
+    <asp:GridView ID="productsGridView" runat="server"></asp:GridView>
+</asp:Content>
+
---
+++ ADO.NET/MicrosoftActiveXDataObject/ADO_101/ASPXs/Products.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Web.UI;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADO_101.ASPXs
+{
+    public partial class Products : Page
+    {
+        private readonly string _connectionString;
+        private readonly SqlConnection _connection;
+        private readonly SqlCommand _command;
+
+        public Products()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["InventoryDbConnectionString"].ConnectionString;
+            _connection = new SqlConnection(_connectionString);
+            _command = new SqlCommand
+            {
+                Connection = _connection
+            };
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadAllProductsGrid();
+            LoadProductCountLabel();
+        }
+
+        private void LoadProductCountLabel()
+        {
+            try
+            {
+                //Poi: Multiple SQL operation MUST BE done from SEPERATE opened connections
+                _connection.Open();
+                totalProductCount.Text = GetAllProductCount().ToString();
+            }
+            catch (Exception) { }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        private void LoadAllProductsGrid()
+        {
+            try
+            {
+                //Poi: Connection needs to be open during data is read from SqlDataReader 
+                //& that is being done while DataBind() is ran over productsGridView
+                _connection.Open();
+
+                productsGridView.DataSource = GetAllProducts();
+                productsGridView.DataBind();
+            }
+            catch (Exception) { }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        //Poi: Even though not obvious at first glance, all execution path is convered
+        private SqlDataReader GetAllProducts()
+        {
+            try
+            {
+                _command.CommandText = @"SELECT * FROM [dbo].[Product]";
+                return _command.ExecuteReader();
+            }
+            catch (Exception) { return null; }
+            finally { }
+        }
+
+        private int? GetAllProductCount()
+        {
+            try
+            {
+                _command.CommandText = @"SELECT COUNT(*) FROM [dbo].[Product]";
+                return Convert.ToInt32(_command.ExecuteScalar());
+            }
+            catch (Exception) { return null; }
+            finally { }
+        }
+    }
+}
