96a84648cb0aa4a2fd99063e8b52e51ca9424e5e
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-03-25 00:40:10 +0600

    Constants are by definition static in C# - To access constant & static class members qualify the member with Type name ONLY. NOT with an instance name

---
+++ RawCode/Constant/Const-101.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Const_101
+{
+    internal static class Client
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Library.BookCount);
+            Console.WriteLine(Library.Address);
+
+            //Poi: Following approaches are NOT valid to access 'static' & 'const' member variables (May be in C++ ?)
+
+            // Console.WriteLine(new Library().BookCount);
+            // Console.WriteLine(new Library().Address);
+        }
+
+        private class Library
+        {
+            //Poi: BookCount is 'const' NOT static. Still 'BookCount' can be invoked with only class name
+            //specified & that's because constants are by definition static (Makes sense !)
+            public const int BookCount = 100;
+            public static string Address = string.Empty;
+        }
+    }
+}
