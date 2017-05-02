8c484bbba186abb5b29eb52ea05ebed7da5d26d2
Sheikh Ashiqul islam <pollob60@live.com>
2017-05-02 23:56:42 +0600

    Hacker Rank Warm Up

---
+++ CompareTheTriplets.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        int aliceComparisonPoint = 0;
+        int bobComparisonPoint = 0;
+        
+        if(a0 > b0) aliceComparisonPoint++;
+        else if(a0 < b0) bobComparisonPoint++;
+            
+        if(a1 > b1) aliceComparisonPoint++;
+        else if(a1 < b1) bobComparisonPoint++;
+            
+        if(a2 > b2) aliceComparisonPoint++;
+        else if(a2 < b2) bobComparisonPoint++;
+    
+        return new int[] { aliceComparisonPoint, bobComparisonPoint };
+    }
+
+    static void Main(String[] args) {
+        string[] tokens_a0 = Console.ReadLine().Split(' ');
+        int a0 = Convert.ToInt32(tokens_a0[0]);
+        int a1 = Convert.ToInt32(tokens_a0[1]);
+        int a2 = Convert.ToInt32(tokens_a0[2]);
+        string[] tokens_b0 = Console.ReadLine().Split(' ');
+        int b0 = Convert.ToInt32(tokens_b0[0]);
+        int b1 = Convert.ToInt32(tokens_b0[1]);
+        int b2 = Convert.ToInt32(tokens_b0[2]);
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
