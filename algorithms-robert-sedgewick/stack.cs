f831c65cfb0ee23d1e60b3656083e37df041c448
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-05-25 23:02:54 +0600

    Naive Stack Implementation

---
+++ algorithms-robert-sedgewick/stack.cs
@@ -0,0 +1,47 @@
+using System;
+
+internal class Stack
+{
+    private System.String[] _sequence;
+    private System.Int32 _hook = 0;
+
+    public Stack(System.Int32 capacity = 10)
+    {
+        _sequence = new System.String[capacity];
+    }
+
+    public bool IsEmpty() => _hook == 0;
+
+    public void Push(System.String item)
+    {
+        _sequence[_hook++] = item;
+    }
+
+    //POI: Will throw exception if invoked after all elements are poped
+    public System.String Pop() => _sequence[--_hook];
+}
+
+internal static class Client
+{
+    internal static void Main()
+    {
+        var stackInstance = new Stack();
+
+        stackInstance.Push("UTF-8");
+        stackInstance.Push("UTF-16");
+        stackInstance.Push("ASCII");
+
+        //POI: LIFO
+        Console.WriteLine(stackInstance.Pop());
+        Console.WriteLine(stackInstance.Pop());
+        Console.WriteLine(stackInstance.Pop());
+
+        //POI: Following will throw Exception
+        //Console.WriteLine(stackInstance.Pop());
+
+        var anotherStackInstance = new Stack();
+
+        //POI: This will throw Exception
+        //Console.WriteLine(anotherStackInstance.Pop());
+    }
+}
