6a8416baaec98d66b89568631080bece32d2b49c
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-08-09 01:07:37 +0600

    Fluent API 101

---
+++ EF/entityframeworktutorial.net/CodeFirst/CommonLib/Model/Course.cs
@@ -0,0 +1,43 @@
+﻿namespace CommonLib.Model
+{
+    using System.Collections.Generic;
+
+    public class Course
+    {
+        public Course()
+        {
+            // POI: It's a better approach to instantiate collections in the constructor
+            // POI: On the other hand this approach is not safe in general because the invoked property is
+            // virtual
+
+            // POI: Don't initialize the other Entity
+            Instructors = new HashSet<Instructor>();
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        // POI: virtual keyword facilities lazy loading
+        public virtual Department Department { get; set; }
+        public virtual ICollection<Instructor> Instructors { get; set; }
+    }
+
+    public class OnlineCourse : Course
+    {
+        public string URL { get; set; }
+    }
+
+    public class OnsiteCourse : Course
+    {
+        public OnsiteCourse()
+        {
+            Details = new Details();
+        }
+
+        // POI: Not levaraging lazy loading because the navigation property is not virtual
+        public Details Details { get; set; }
+    }
+}
