bc7e831c626a92990d1030765dfc4d1d069d97e2
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-02-09 23:54:42 +0600

    Cookie Premier

---
+++ Web-api/101/OneOOne/MVCEndPoint/Controllers/CookieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEndPoint.Controllers
+{
+    public class CookieController : Controller
+    {
+        // GET: Cookie
+        public ActionResult Index()
+        {
+            var kCookie = "uCookie";
+
+            //var eCookie = Request.Cookies.Get(kCookie);
+
+            //if (eCookie == null) Response.Cookies.Add(new HttpCookie(kCookie, DateTime.Now.ToString()));
+            //else ViewBag.ECookie = eCookie.Value;
+
+            Response.Cookies.Add(new HttpCookie(kCookie, DateTime.Now.ToString()));
+
+            return View();
+        }
+    }
+}
---
+++ Web-api/101/OneOOne/MVCEndPoint/Controllers/QueryStringEncoderCheckerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEndPoint.Controllers
+{
+    public class QueryStringEncoderCheckerController : Controller
+    {
+        // GET: QueryStringEncoderChecker
+
+        [ValidateInput(false)]
+        public ActionResult Index()
+        {
+            var u = Request.QueryString["user"];
+            var i = Request.QueryString["nid"];
+            var h = Request.QueryString["htmlInput"];
+
+            ViewBag.user = u;
+            ViewBag.nid = i;
+            ViewBag.Html = h;
+
+            //var eCookie = Request.Cookies.Get("uCookie");
+            //if (eCookie != null) ViewBag.ECookie = eCookie.Value;
+
+            var kCookie = "uCookie";
+            Response.Cookies.Add(new HttpCookie(kCookie, DateTime.Now.ToString()));
+
+            return View();
+        }
+    }
+}
