8f3923ed871aef50a30fbbd0b23b4d63492f881d
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-02-11 23:57:52 +0600

    Item - 34

---
+++ Effective CSharp/Ch-2/Item-34.cs
@@ -0,0 +1,37 @@
+using System;
+
+class T
+{
+	public static void Main()
+	{
+		var o = new D();
+
+		// POI: Better match for this overload is B.Foo(D2...) but that's not invoked because lookup starts from the derived class
+		// Only when a match is not found base class is looked for matching.
+		// POI: In this case, a matching (albeit not the best matching) is found in the derived class so control doesn't look into 
+		// the base class
+		o.Foo(new D2());// D.Foo
+		o.Foo(new B2());// D.Foo
+
+	}
+}
+
+class B2 { }
+class D2 : B2 { }
+
+class B
+{
+	public void Foo(D2 p)
+	{
+		Console.WriteLine("B.Foo");
+	}
+}
+
+class D : B
+{
+	public void Foo(B2 p)
+	{
+		Console.WriteLine("D.Foo");
+	}
+}
+
