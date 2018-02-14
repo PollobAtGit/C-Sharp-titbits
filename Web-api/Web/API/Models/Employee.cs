d2b09c0b855c418e9749273c414174ea1583c944
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-02-14 23:42:15 +0600

    Extract

---
+++ Web-api/Web/API/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Controllers.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int OrgId { get; set; }
+    }
+}
