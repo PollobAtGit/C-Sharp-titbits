ed5c05b73caed4c4f4d46162d45b25f97176b78a
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-03-04 02:03:19 +0600

    Ch-3 Start

--- EF-Julia Lerman/EF-DbContext/Ch-Three/AddEntity.cs
+++ EF-Julia Lerman/EF-DbContext/Ch-Three/AddEntity.cs
@@ -1,16 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ObjectModel.DAL;
+using ObjectModel.Model;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ch_Three
 {
     static class AddEntity
     {
+        static Action<object> cl = (object x) => Console.WriteLine(x);
+
         public static void Main()
         {
+            Page_44Async();
+        }
 
+        private static void Page_44Async()
+        {
+            // TODO: Why EntityFramework is needed when only dependency is on BreakAwayContext? 'Is A' relationship?
+            using (var context = new BreakAwayContext())
+            {
+                context.Destinations.ToList().ForEach(x => cl(x.Name));
+
+                // POI: This operation goes to DB even if data is available in Local
+                // POI: It's such because operaiton is being done against DbSet<T>
+                if (!context.Destinations.Any(x => x.DestinationId == 4))
+                {
+                    var d = new Destination
+                    {
+                        Name = "Maldives",
+                        Description = "Paradise",
+                        Country = "Bangladesh"
+                    };
+
+                    context.Destinations.Add(d);
+
+                    cl("Saving Changes");
+
+                    // TODO: Why await/async not working here?
+                    context.SaveChanges();
+
+                    cl(d.DestinationId); // 4
+                }
+            }
         }
     }
 }
